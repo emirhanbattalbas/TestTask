baa1e4e6845164dd5307a06d59f5ff092051ff76
emir_battal <emirhan.battalbas@mobillium.com>
2021-02-20 13:44:10 +0300

    TensorFlow pod install and model file added.

--- TestTask/MainController/MainViewController.swift
+++ TestTask/MainController/MainViewController.swift
@@ -1,10 +1,30 @@
 import UIKit
+import AVFoundation
+import TensorFlowLite
+import TensorFlowLiteC
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        showCamera()
     }
-
+    
+    private func showCamera() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
+
+// MARK: - UIImagePickerControllerDelegate
+extension MainViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) { }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) { }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension MainViewController: UINavigationControllerDelegate { }
