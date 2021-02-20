73314f754bd3d4b3d5282aed0e7df3a5f18ca0b9
emir_battal <emirhan.battalbas@mobillium.com>
2021-02-20 10:40:35 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Emirhan Battalbaş on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
