985e24610e5e1842624bf6f4bafca8ab8d6b3ffc
emir_battal <emirhan.battalbas@mobillium.com>
2021-02-20 12:06:25 +0300

    initialize project

--- TestTask/App/AppDelegate.swift
+++ TestTask/App/AppDelegate.swift
@@ -10,10 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow()
+        self.window?.backgroundColor = .clear
+        self.window?.makeKeyAndVisible()
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+    //    navigationController.isNavigationBarHidden = true
+        self.window?.rootViewController = navigationController
         return true
     }
 
